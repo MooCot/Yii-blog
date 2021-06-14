ee63d0437540401753e6711aa93804970805cade
morrowind <gitmooncat@gmail.com>
2021-06-14 20:12:52 +0300

    edit migration

--- blog/migrations/m210614_165940_create_category_table.php
+++ blog/migrations/m210614_165940_create_category_table.php
@@ -14,6 +14,7 @@ class m210614_165940_create_category_table extends Migration
     {
         $this->createTable('{{%category}}', [
             'id' => $this->primaryKey(),
+						'title'=>$this->string()
         ]);
     }
 
