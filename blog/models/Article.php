c3be1e19262345ac87c928a93c9374a869502ee9
morrowind <gitmooncat@gmail.com>
2021-06-15 01:42:49 +0300

    add tag functions

--- blog/models/Article.php
+++ blog/models/Article.php
@@ -3,6 +3,8 @@
 namespace app\models;
 
 use Yii;
+use yii\data\Pagination;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "article".
@@ -91,7 +93,7 @@ class Article extends \yii\db\ActiveRecord
     {
         return $this->hasOne(Category::className(), ['id' => 'category_id']);
     }
-		
+
 		public function saveCategory($category_id)
     {
         $category = Category::findOne($category_id);
@@ -100,6 +102,37 @@ class Article extends \yii\db\ActiveRecord
             $this->link('category', $category);
             return true;            
         }
+    }
+
+		public function getTags()
+    {
+        return $this->hasMany(Tag::className(), ['id' => 'tag_id'])
+            -> viaTable('article_tag', ['article_id' => 'id']);
+    }
+
+		public function getSelectedTags()
+    {
+        $selectedIds = $this->getTags()->select('id')->asArray()->all();
+        return ArrayHelper::getColumn($selectedIds, 'id');
+    }
+
+		public function saveTags($tags)
+    {
+        if (is_array($tags))
+        {
+            $this->clearCurrentTags();
+
+            foreach($tags as $tag_id)
+            {
+                $tag = Tag::findOne($tag_id);
+                $this->link('tags', $tag);
+            }
+        }
+    }
+
+		public function clearCurrentTags()
+    {
+        ArticleTag::deleteAll(['article_id' => $this->id]);
     }
 
 }
--- blog/modules/admin/controllers/ArticleController.php
+++ blog/modules/admin/controllers/ArticleController.php
@@ -5,6 +5,7 @@ namespace app\modules\admin\controllers;
 use app\models\Article;
 use app\models\ArticleSearch;
 use app\models\ImageUpload;
+use app\models\Tag;
 use Yii;
 use yii\filters\VerbFilter;
 use yii\web\Controller;
@@ -161,11 +162,29 @@ class ArticleController extends Controller
         }
 
         return $this->render('category', [
-            'article'=>$article,
-            'selectedCategory'=>$selectedCategory,
-            'categories'=>$categories
+            'article' => $article,
+            'selectedCategory' => $selectedCategory,
+            'categories' => $categories
         ]);
 
-				
+    }
+
+		public function actionSetTags($id)
+    {
+        $article = $this->findModel($id);
+        $selectedTags = $article->getSelectedTags(); //
+        $tags = ArrayHelper::map(Tag::find()->all(), 'id', 'title');
+
+        if(Yii::$app->request->isPost)
+        {
+            $tags = Yii::$app->request->post('tags');
+            $article->saveTags($tags);
+            return $this->redirect(['view', 'id' => $article->id]);
+        }
+        
+        return $this->render('tags', [
+            'selectedTags' => $selectedTags,
+            'tags' => $tags
+        ]);
     }
 }
